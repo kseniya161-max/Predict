a7229e352094633164f783fa2ea1a95ea01b8434
Kseniya <baharevaxen@yandex.ru>
2026-08-18 22:32:59 +0300

    creating Statistic model

--- models.py
+++ models.py
@@ -1,10 +1,5 @@
-# id
-# home_team
-# away_team
-# match_date
-# status
-# competition
-from sqlalchemy import String, DateTime
+
+from sqlalchemy import String, DateTime, BigInteger, Integer
 from sqlalchemy.orm import Mapped, mapped_column
 
 from database import Base
@@ -20,14 +15,16 @@ class Match(Base):
     competition:Mapped[str] = mapped_column(String(100))
 
 
+class Statistic(Base):
+    __tablename__ = 'statistic'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    team: Mapped[str] = mapped_column(String(100))
+    wins: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+    draws: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+    losses: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+    goals_scored: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+    goals_conceded: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
 
-
-# team
-# wins
-# draws
-# losses
-# goals_scored
-# goals_conceded
 #
 #
 # id
