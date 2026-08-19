cf06b6372405f9c3510f7179a8c52fdd70438458
Kseniya <baharevaxen@yandex.ru>
2026-08-19 16:11:24 +0300

    creating endpoint get_matches

--- routers.py
+++ routers.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from sqlalchemy import select
 
 from database import AsyncSessionLocal
-from models import Statistic
+from models import Statistic, Match
 from services import save_matches, save_statistics
 
 router = APIRouter(prefix="/matches", tags=["matches"])
@@ -26,6 +26,16 @@ async def get_statistics():
     async with AsyncSessionLocal() as session:
         result = await session.scalars(
             select(Statistic)
+        )
+
+        return result.all()
+
+
+@router.get("/matches")
+async def get_matches():
+    async with AsyncSessionLocal() as session:
+        result = await session.scalars(
+            select(Match)
         )
 
         return result.all()
