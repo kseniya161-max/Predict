d1249d8317aa684fb041c984f829408ec0867b0e
Kseniya <baharevaxen@yandex.ru>
2026-08-19 13:20:48 +0300

    correcting env.py

--- alembic/env.py
+++ alembic/env.py
@@ -5,6 +5,10 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from config import settings
+from database import Base
+from models import Match
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -60,9 +64,10 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
+    from sqlalchemy import create_engine
+
+    connectable = create_engine(
+        settings.DATABASE_URL,
         poolclass=pool.NullPool,
     )
 
