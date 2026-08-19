7cbd3a0a86e41e7a897b96e801d61d33ff1b88a8
Kseniya <baharevaxen@yandex.ru>
2026-08-19 16:06:32 +0300

    creating endpoint def get_statistics()

--- routers.py
+++ routers.py
@@ -1,11 +1,31 @@
 from fastapi import APIRouter
+from sqlalchemy import select
 
-from services import save_matches
+from database import AsyncSessionLocal
+from models import Statistic
+from services import save_matches, save_statistics
 
 router = APIRouter(prefix="/matches", tags=["matches"])
+
 
 
 @router.post("/load")
 async def load_matches():
     await save_matches()
     return {"messages": "matches are loaded successfully"}
+
+
+@router.post("/statistics")
+async def update_statistics():
+    await save_statistics()
+    return {"message": "Statistics saved successfully"}
+
+
+@router.get("/statistics")
+async def get_statistics():
+    async with AsyncSessionLocal() as session:
+        result = await session.scalars(
+            select(Statistic)
+        )
+
+        return result.all()
